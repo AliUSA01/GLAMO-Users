11852641789b236a859391beabde30783739c16d
AliUSA01 <alimammadov873@gmail.com>
2025-05-30 23:56:58 +0400

    security

--- src/main/java/az/glamouserservice/security/UserDetailsServiceImpl.java
+++ src/main/java/az/glamouserservice/security/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package az.glamouserservice.security;
 
 import az.glamouserservice.dao.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserSecurityDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .map(UserSecurityDetails::new)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
     }
 }
